b67fd6faf244ecfafe1ebc73b2e72462fc9b1a9d
langally <langally@ukr.net>
2020-07-13 23:30:30 +0300

    change city

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Oles Kurivchak");
         System.out.println("ul. Prylinskiego 32/125");
-        System.out.println("30-376, Krakow");
+        System.out.println("30-376, Krakmmmow");
 
     }
 }
